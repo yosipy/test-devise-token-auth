49af944b35b6eb2408c3434d4103eb17ef7b8e92
yosi <todacchi.desu@gmail.com>
2021-07-24 17:05:11 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :v1 do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
